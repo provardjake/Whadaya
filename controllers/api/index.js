8cf8d90eaa90576e4e5c59e851099e8efc01c527
Newtron21 <annacpnewton@gmail.com>
2023-09-26 15:27:37 -0600

    fixing naming conventions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
 router.use('/review', reviewRoutes);
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -24,6 +24,14 @@ User.init(
                 len: [3, 50]
             }
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+          },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -34,10 +42,14 @@ User.init(
     },
     {
         hooks: {
-            beforeCreate: async (newUserData)=>{
+            beforeCreate: async (newUserData) => {
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
-            }
+              },
+              beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+              },
         },
         sequelize,
         timestamps: false,
