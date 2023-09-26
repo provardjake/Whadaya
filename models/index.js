6d1b60a8a95fc353fb363c50d783d6854d95b41d
Jake Provard <provardjake@gmail.com>
2023-09-25 21:07:20 -0600

    updated index.js to connect the models

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const User = require("./User");
+const Review = require("./Review");
+const Categories = require("./Categories");
+const Comment = require("./Comment");
+
+User.hasMany(Review,{
+    foreignKey: "user_id"
+});
+
+Review.belongsTo(User,{
+    foreignKey: "user_id"
+});
+
+User.hasMany(Comment,{
+    foreignKey: "user_id"
+});
+
+Comment.belongsTo(User,{
+    foreignKey: "user_id"
+});
+
+Categories.hasMany(Review,{
+    foreignKey: "category_id"
+});
+
+Review.belongsTo(Categories,{
+    foreignKey: "category_id"
+});
+
+Review.hasMany(Comment,{
+    foreignKey: "review_id"
+});
+
+Comment.belongsTo(Review,{
+    foreignKey: "review_id"
+});
