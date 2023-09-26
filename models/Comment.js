855bed84894184e2d84434035cc0e3a5f2e67f9c
Jake Provard <provardjake@gmail.com>
2023-09-25 20:29:20 -0600

    set up models for categories, comment, review and user

---
+++ models/Comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model{}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        message: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        likes: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        replies: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        review_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "review",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment",
+    }
+)
